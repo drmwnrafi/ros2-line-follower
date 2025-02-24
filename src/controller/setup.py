6826bee1b59e6e447acff95f6f907943c9530299
drmwnrafi <drmwnnrafi@gmail.com>
2025-02-24 07:37:56 +0700

    initial commit

---
+++ src/controller/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Controller package that simulates an 8-bit magnetic line follower by sampling image_raw in 8 chunks',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller_node = controller.ctrl_node:main'
+        ],
+    },
+)
